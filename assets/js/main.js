0210fb6acf6431c7f971cbc13e3fa201c86cf989
slopez <xperialopez1989@gmail.com>
2022-11-11 18:42:37 -0300

    proyectoFinal v1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,345 +1,289 @@
+const colores = ["rojo", "verde", "azul", "amarillo"];
 
-/****************************************************************************************************************
-
-NOTAS:
-
-Necesito utilizar .addEventListener para escuchar a que color el jugador hace click, esos clicks deberían guardarse en un array "jugada", ese array tiene que ser comparado indice a indice con otro array "jugadaMaquina".
-Que es "jugada"? es un array que va incorporando las selecciones del jugador. Tiene que haber una variable "seleccionado" que vaya tomando cada eleccion del jugador y una función que vaya incorporando los datos de "seleccionado" en el array "jugada"
-
-Primero que nada tengo que lograr que el array jugada funcione y que incorpore cada una de las jugadas del jugador. 
-
-Estudiar setTimeOut se usa en cada solución que vi. También estudiar event.target.dataset
-Ver de poner un boton "comenzar"
-
-Extracto de proyecto similar: The humanTurn() function needs to be executed after the computer’s sequence is over so we cannot call it immediately. We need to add an artificial delay and calculate when the computer will be done with the sequence of button taps.
-
-RESUMEN:
-
-1. Necesito que se genere un valor aleatorio y se guarde en el array "jugadaMaquina" HECHO
-2. Luego necesito que el programa se PAUSE y espere a que el jugador ingrese un color semi HECHO
-3. Ese valor elegido debe guardarse en el array "jugada" HECHO
-4. El programa continúa y va comparando cada ingreso del jugador con cada valor del indice "jugadaMaquina"
-5. Si se comete un error el juego termina, si los array son iguales se vuelve al paso 1.
-
-Se me ocurre una idea alternativa, que las jugadas de la máquina se generen de primera todas juntas, luego los clicks del jugador se van comparando con cada indice.
-
-
-
-
-****************************************************************************************************/
-
-
-
-class jugador {
-  constructor(nombre, ciudad, puntaje) {
-    this.nombre = nombre;
-    this.ciudad = ciudad;
-    this.puntaje = puntaje;
-  }
-}
-
-const jugador1 = new jugador("Mariano", "Buenos Aires", 7);
-const jugador2 = new jugador("Zeca", "Rio de Janeiro", 8);
-const jugador3 = new jugador("Jhon", "Miami", 4);
-const jugador4 = new jugador("Radamel", "Bogotá", 10);
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-let esRojo=false;
-let esVerde=false;
-let esAzul=false;
-let esAmarillo=false;
+let audioRojo = document.getElementById("audioRojo");
+let audioVerde = document.getElementById("audioVerde");
+let audioAzul = document.getElementById("audioAzul");
+let audioAmarillo = document.getElementById("audioAmarillo");
+let click = document.querySelectorAll(".color");
+let ronda = 0;
 let jugada = [];
 let jugadaMaquina = [];
-let valores = document.querySelectorAll(".color");
-console.log(valores)
+let jugadasSesion = [];
+let nombreIngresado = "";
+let contador = 0;
 
-function quitarSeleccionado(color) {
-  let elemento = document.querySelector(color);elemento.classList.remove(`seleccionado`);
+//Comienzo del juego.
+
+document
+  .querySelector("#start")
+  .addEventListener("click", function comenzarJuego() {
+    document
+    .querySelector("#start")
+    .classList.add("animate__zoomOut");
+    quitarPrevioInicio();
+    setTimeout(() => {
+      proximaSecuencia();  
+    }, 500);
+  });
+
+//funcion para quitar blur inicial
+
+function quitarPrevioInicio() {
+  let botones = document.querySelectorAll(".color");
+  botones.forEach((element) => {
+    element.classList.remove("previoInicio");
+  });
 }
 
-quitarSeleccionado('.rojo')
-quitarSeleccionado('.verde')
-quitarSeleccionado('.azul')
-quitarSeleccionado('.amarillo')
+//funcion para agregar blur inicial
 
-
-///////////////////////////JUGADAS MAQUINA
-
-let generarRondas = (e) => {
-  let rondas = e.target.value
-  for (i=1;i<=rondas;i++) {
-
-    let aleatorioEntero = Math.floor(Math.random() * valores.length);
-    jugadaMaquina.push(aleatorioEntero);
-    }
-    console.log(jugadaMaquina)
-    mostrarRonda()
+function agregarPrevioInicio() {
+  let botones = document.querySelectorAll(".color");
+  botones.forEach((element) => {
+    element.classList.add("previoInicio");
+  });
 }
 
-function mostrarRonda() {
-  let i = 0
-  let movimientos = setInterval(function () {
-    jugadaMaquina[i];
-    
-    i++;
-    if (i>= jugadaMaquina.length) {
-      clearInterval(movimientos);
-      switch (i) {
-        case 0:
-          quitarSeleccionado('.rojo')
-        break;
-        case 1:
-          quitarSeleccionado('.verde')
-        break;
-        case 2:
-          quitarSeleccionado('.azul')
-        break;
-        case 3:
-          quitarSeleccionado('.amarillo')
-        break;
-      }
-    }
-  }, 600)
+//funcion para generar aleatorio incorporarlo al array jugadaMaquina.
+
+async function proximaSecuencia() {
+  jugada = [];
+  let aleatorioEntero = colores[Math.floor(Math.random() * colores.length)];
+  jugadaMaquina.push(aleatorioEntero);
+  mostrarSecuencia(jugadaMaquina);
 }
 
+// funcion para reproducir audio
 
-
-
-document.addEventListener(`input`,generarRondas)
-
-////////////////////////////JUGADAS PERSONA
-
-// necesito una funcion que registre los clicks del jugador
-
-function clickPersona() {
-  
-  document.querySelector(".rojo").addEventListener("click",function() {
-    esRojo = true;
-    incorporarSeleccionado()
-    if (jugada.length === jugadaMaquina.length) {
-      console.log(jugada)
-      comparar()
-    }
-  }) 
-  document.querySelector(".verde").addEventListener("click",function() {
-    esVerde == true;
-    incorporarSeleccionado()
-    if (jugada.length === jugadaMaquina.length) {
-      console.log(jugada)
-      comparar()
-    }
-  })
-  document.querySelector(".azul").addEventListener("click",function() {
-    esAzul = true;
-    incorporarSeleccionado()
-    if (jugada.length === jugadaMaquina.length) {
-      console.log(jugada)
-      comparar()
-    }
-  })
-  document.querySelector(".amarillo").addEventListener("click",function() {
-    esAmarillo = true;
-    incorporarSeleccionado()
-    if (jugada.length === jugadaMaquina.length) {
-      console.log(jugada)
-      comparar()
-    }
-  })
-  } 
-
-// necesito una funcion que dependiendo cual fue el click del usuario incorpore ese valor al array jugada.
-
-function incorporarSeleccionado() {
-
-    if (esRojo == true) {
-      esRojo = false
-      return jugada.push(0);  
-    } else if (esVerde == true) {
-      esVerde = false
-      return jugada.push(1)
-    } else if (esAzul == true) {
-      esAzul = false
-      return jugada.push(2)
-    } else if (esAmarillo == true) {
-      esAmarillo = false
-      return jugada.push(3)
+function reproducirAudio(id) {
+  switch (id) {
+    case "rojo":
+      audioRojo.play();
+      break;
+    case "verde":
+      audioVerde.play();
+      break;
+    case "azul":
+      audioAzul.play();
+      break;
+    case "amarillo":
+      audioAmarillo.play();
+      break;
   }
 }
- 
 
-// necesito una funcion que compare el array jugada con jugadaMaquina indice a indice, o al menos luego de finalizado el length correspondiente a esa ronda. La funcion deberia tomar como parametro el numero de ronda en la que estoy. A su vez al finalizar cada ronda, debe vaciar el array jugada.
+// promise para reproducir funcion de audio con delay.
+
+const delaySonido = (color) => {
+  return new Promise((resolve) =>
+    setTimeout(() => {
+      resolve(reproducirAudio(color));
+    }, 100)
+  );
+};
+
+// funcion para mostrar la secuencia del array jugadaMaquina.
+
+function mostrarSecuencia(arr) {
+  arr.forEach((element, i) => {
+    setTimeout(() => {
+      reproducirAudio(element);
+      prenderApagarBoton(element);
+    }, (i + 1) * 400);
+  });
+}
+
+// escuchador que registra los clicks del jugador.
 
 
-  
+click.forEach((element) => {
+  element.addEventListener("click", (event) => {
+    event.preventDefault();
+    let id = event.target.id;
+    reproducirAudio(id);
+    prenderApagarBoton(id);
+    jugada.push(id);
+    comparar(jugada, jugadaMaquina);
+  });
+});
 
-  function comparar() {
-    if (jugada.length === jugadaMaquina.length) {
-      let comparacion = jugada.every((valor,indice) => valor === jugadaMaquina[indice])
-      if (comparacion) {
-        return alert(`Excelente, lo lograste.`)
-      } else {
-        return alert(`Perdiste..`)
+
+
+// funcion prender y apagar boton al hacer click
+
+function prenderApagarBoton(id) {
+  document.getElementById(id).classList.add("seleccionado");
+  setTimeout(function () {
+    document.getElementById(id).classList.remove("seleccionado");
+  }, 200);
+}
+
+// funcion que compare el array jugada con jugadaMaquina indice a indice. Si el jugador falla, se reinicia el juego.
+
+function comparar(arr1, arr2) {
+  let comparacion = arr1.every((valor, indice) => valor === arr2[indice]);
+  if (!comparacion) {
+    let timerInterval;
+    Swal.fire({
+      title: `Perdiste.. tu puntaje fue ${ronda}`,
+      timer: 2000,
+      timerProgressBar: true,
+      didOpen: () => {
+        Swal.showLoading();
+        const b = Swal.getHtmlContainer().querySelector("b");
+      },
+      willClose: () => {
+        clearInterval(timerInterval);
+      },
+    }).then((result) => {
+      if (result.dismiss === Swal.DismissReason.timer) {
       }
-    }
+    });
+    jugadasSesion.push(ronda);
+    guardarJugada();
+    ronda = 0;
+    jugada = [];
+    jugadaMaquina = [];
+    agregarPrevioInicio();
+    document.querySelector("#fondoPokemon").style.visibility = "visible";
+    setTimeout(() => {
+      document.querySelector("#fondoPokemon").style.visibility = "hidden";
+    }, 6000);
+    setTimeout(() => {
+      document.querySelector(".pokemon").style.visibility = "visible";
+    }, 2000);
+    setTimeout(() => {
+      document.querySelector(".pokemon").style.visibility = "hidden";
+    }, 4000);
+    setTimeout(() => {
+      document.querySelector(".comentarioPokemon").style.visibility = "visible";
+    }, 4000);
+    setTimeout(() => {
+      document.querySelector(".comentarioPokemon").style.visibility = "hidden";
+    }, 6000);
+    setTimeout(() => {
+      document.querySelector("#start").classList.remove("animate__zoomOut");
+    }, 6000);
+    cargarJugada();
+  } else if (arr1.length == arr2.length) {
+    ronda++;
+    proximaSecuencia();
   }
-  
-clickPersona()
+}
+
+// funcion para guardar jugada en local storage y escuchar el local storage al cargar sitio.
+
+function guardarJugada() {
+  localStorage.setItem("puntajesSesion", JSON.stringify(jugadasSesion));
+}
+
+function cargarJugada() {
+  jugadasSesion = JSON.parse(localStorage.getItem("puntajesSesion"));
+  mejorPuntaje();
+  puntajePromedio();
+  cantidadJugadas();
+};
+
+//funcion para guardar nombre en local storage y escuchar el local storage al cargar sitio.
+
+function guardarNombre() {
+  localStorage.setItem("nombreJugador", nombreIngresado);
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  nombreIngresado = localStorage.getItem("nombreJugador");
+  imprimirNombre();
+});
+
+// funcion que pregunta nombre con prompt.
+
+function preguntaNombre() {
+  nombreIngresado = prompt("Cuál es tu nombre?");
+    imprimirNombre();
+    guardarNombre();
+  }
+
+// funcion retorno ingresara nombre en pantalla
+
+function imprimirNombre() {
+  let element = document.querySelector('.nombre');
+  element.innerHTML = `
+  <p>${nombreIngresado}</p>
+  `
+}
+
+// funciones para imprimir estadisticas en pantalla
+
+  function mejorPuntaje() {
+    let element = document.querySelector(".mejorPuntaje");
+      element.innerHTML = `
+    <p>mejor puntaje: ${(Math.max(...jugadasSesion))}</p>;
+    `
+    } 
+  function cantidadJugadas() {
+    let element = document.querySelector(".cantidadJugadas");
+    element.innerHTML = `
+    <p>cantidad jugadas: ${jugadasSesion.length}</p>;
+    `
+  }
+  function puntajePromedio() {
+    for( let i = 0 ; i <= jugadasSesion.length - 1 ; i++) {
+      contador += jugadasSesion[i]
+    }
+    let element = document.querySelector(".puntajePromedio");
+    element.innerHTML = `
+    <p>puntaje promedio: ${Math.round(contador / jugadasSesion.length)}</p>;
+    `
+  }
+
+// funcion borrar datos del storage
+
+document.querySelector(".borrarStorage").addEventListener("click", function () {
+  localStorage.clear();
+  jugadasSesion = [];
+  mejorPuntaje();
+  cantidadJugadas();
+  puntajePromedio();
+  preguntaNombre();
+});
+
+// probando Fetch random con pokemones
+
+let pokemonAleatorio = Math.round(Math.random() * 800);
+
+const url = `https://pokeapi.co/api/v2/pokemon/${pokemonAleatorio}/`;
+
+fetch(url)
+  .then((response) => response.json())
+  .then((data) => {
+    let element = document.querySelector(".pokemon");
+    element.innerHTML = `
+      <img src='${data.sprites.front_default}'/>
+      <p>${data.name}</p>
+    `
+    document.querySelector(".pokemon img").style.width = "200px";
+    ;
+  })
 
 /*
- Que me falta?
-
- En cada jugada debe marcarse el indice de la maquina.
- Si supero el primer nivel, el juego debe adaptar la comparacion al segundo nivel y luego al tercer nivel.
- Aplicar JSON
-
- */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* document.querySelector(".verde").addEventListener("click",function() {
-  jugada.push(1)
-  console.log(jugada)
-  if (jugada[0] == jugadaMaquina[0]) {
-    console.log("acertaste")
-    jugada = []
-  } else {
-    console.log("perdiste")
-  }
-})
-
-document.querySelector(".azul").addEventListener("click",function() {
-  jugada.push(2)
-  console.log(jugada)
-  if (jugada[0] == jugadaMaquina[0]) {
-    console.log("acertaste")
-    jugada = []
-  } else {
-    console.log("perdiste")
-  }
-})
-
-document.querySelector(".amarillo").addEventListener("click",function() {
-  jugada.push(3)
-  console.log(jugada)
-  if (jugada[0] == jugadaMaquina[0]) {
-    console.log("acertaste")
-    jugada = []
-  } else {
-    console.log("perdiste")
-  }
-}) */
-
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* //JUGADA PERSONA 1
-
-document.querySelector(".rojo").addEventListener("click",function() {
-  jugada.push(0)
-  console.log(jugada)
-  if (jugada[0] == jugadaMaquina[0]) {
-    console.log("acertaste")
-    jugada = []
-  } else {
-    console.log("perdiste")
-  }
-})
-
-document.querySelector(".verde").addEventListener("click",function() {
-  jugada.push(1)
-  console.log(jugada)
-  if (jugada[0] == jugadaMaquina[0]) {
-    console.log("acertaste")
-    jugada = []
-  } else {
-    console.log("perdiste")
-  }
-})
-
-document.querySelector(".azul").addEventListener("click",function() {
-  jugada.push(2)
-  console.log(jugada)
-  if (jugada[0] == jugadaMaquina[0]) {
-    console.log("acertaste")
-    jugada = []
-  } else {
-    console.log("perdiste")
-  }
-})
-
-document.querySelector(".amarillo").addEventListener("click",function() {
-  jugada.push(3)
-  console.log(jugada)
-  if (jugada[0] == jugadaMaquina[0]) {
-    console.log("acertaste")
-    jugada = []
-  } else {
-    console.log("perdiste")
-  }
-})
-
-
-
-/* new Promise(() => {
-  document.querySelector(".rojo").addEventListener("click",seleccionado)
-    
-  function seleccionado () {
-    consolidado.push(0)
-  }
-
-})
- */
-
-
-
+PENDIENTES
+
+- OK Crear funcion y aplicar promise a los sonidos
+- OK Reducir funcion clickPersona
+- OK Cambiar sonidos
+- OK Dar estilo
+- OK Ver de utilizar alguna libreria, si uso libreria intentar minificarla. Podria usar toastify para niveles.
+- OK Hay que utilizar FETCH.
+- OK Terminar el catch del FETCH.
+- OK Aplicar local storage para guardar jugadas, y borrar storage.
+- OK Colocar prompt de nombre solo si el storage está vacío.
+- OK Aplicar algun operador ternario.
+- OK Agrandar pokemons
+-    Hacer objetos.
+-    Desactivar clicks mientras se muestra jugadaMaquina NO PUDE.
+-    Quitar infinity.
+-    Ver de aplicar spread operator.
+-    Preguntar nombre al inicio cuando no haya storage.
+-    Hacer responsive.
+
+*/
--- assets/js/pruebas.js
+++ assets/js/pruebas.js
@@ -1,4 +1,4 @@
-/* const valores = ["peras", "manzanas", "banana"]
+/* /* const valores = ["peras", "manzanas", "banana"]
 const aleatorio = valores [Math.floor(Math.random() * valores.length)]
 
 console.log (valores.length)
@@ -82,7 +82,7 @@ let cuatro = 0;
 
 ****************************************************************************************************/
 
-let inicio = confirm(
+/* let inicio = confirm(
   "Bienvenido a 'La versión barata de Simón Dice!' querés jugar?"
 );
 class jugador {
@@ -167,7 +167,7 @@ if (inicio == true) {
 } else {
   alert("Ok, vos te lo perdés.");
 }
-
+ */
 
 
 /* 
@@ -188,3 +188,408 @@ Ante cualquier duda quedo atento.
 CORREGIDO
 
 */
+
+
+
+/* function resolveAfter2Seconds() {
+  console.log("starting slow promise");
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("slow");
+      console.log("slow promise is done");
+    }, 2000);
+  });
+}
+resolveAfter2Seconds() */
+
+/****************************************************************************************************************
+
+NOTAS:
+
+Necesito utilizar .addEventListener para escuchar a que color el jugador hace click, esos clicks deberían guardarse en un array "jugada", ese array tiene que ser comparado indice a indice con otro array "jugadaMaquina".
+Que es "jugada"? es un array que va incorporando las selecciones del jugador. Tiene que haber una variable "seleccionado" que vaya tomando cada eleccion del jugador y una función que vaya incorporando los datos de "seleccionado" en el array "jugada"
+
+Primero que nada tengo que lograr que el array jugada funcione y que incorpore cada una de las jugadas del jugador. 
+
+Estudiar setTimeOut se usa en cada solución que vi. También estudiar event.target.dataset
+Ver de poner un boton "comenzar"
+
+Extracto de proyecto similar: The humanTurn() function needs to be executed after the computer’s sequence is over so we cannot call it immediately. We need to add an artificial delay and calculate when the computer will be done with the sequence of button taps.
+
+RESUMEN:
+
+1. Necesito que se genere un valor aleatorio y se guarde en el array "jugadaMaquina" HECHO
+2. Luego necesito que el programa se PAUSE y espere a que el jugador ingrese un color semi HECHO
+3. Ese valor elegido debe guardarse en el array "jugada" HECHO
+4. El programa continúa y va comparando cada ingreso del jugador con cada valor del indice "jugadaMaquina"
+5. Si se comete un error el juego termina, si los array son iguales se vuelve al paso 1.
+
+Se me ocurre una idea alternativa, que las jugadas de la máquina se generen de primera todas juntas, luego los clicks del jugador se van comparando con cada indice.
+
+
+
+
+****************************************************************************************************/
+
+
+
+/* class jugador {
+  constructor(nombre, ciudad, puntaje) {
+    this.nombre = nombre;
+    this.ciudad = ciudad;
+    this.puntaje = puntaje;
+  }
+}
+
+const jugador1 = new jugador("Mariano", "Buenos Aires", 7);
+const jugador2 = new jugador("Zeca", "Rio de Janeiro", 8);
+const jugador3 = new jugador("Jhon", "Miami", 4);
+const jugador4 = new jugador("Radamel", "Bogotá", 10); */
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/* let ronda = 0;
+let esRojo=false;
+let esVerde=false;
+let esAzul=false;
+let esAmarillo=false;
+let jugada = [];
+let jugadaMaquina = [];
+
+let valores = document.querySelectorAll(".color");
+console.log(valores)
+
+function quitarSeleccionado(color) {
+  let elemento = document.querySelector(color);elemento.classList.remove(`seleccionado`);
+}
+
+quitarSeleccionado('.rojo')
+quitarSeleccionado('.verde')
+quitarSeleccionado('.azul')
+quitarSeleccionado('.amarillo') */
+
+
+///////////////////////////JUGADAS MAQUINA
+
+/* function comparando() {  */
+  // genero un aleatorio en jugadaMaquina
+  /* let aleatorioEntero = Math.floor(Math.random() * valores.length);
+  jugadaMaquina.push(aleatorioEntero);
+  console.log(jugadaMaquina) */
+  // muestro la jugadaMaquina
+  //mostrarRonda()
+  // la maquina espera a que el jugador copie la ronda
+  /* return new Promise((resolve)=>{
+    clickPersona()
+      resolve(comparar())
+    
+  })
+    }
+
+  comparando()
+  comparar() */
+
+/* let generarRondas = (e) => {
+  let rondas = e.target.value
+  for (i=1;i<=rondas;i++) {
+
+    let aleatorioEntero = Math.floor(Math.random() * valores.length);
+    jugadaMaquina.push(aleatorioEntero);
+    }
+    console.log(jugadaMaquina)
+    mostrarRonda()
+}
+
+function mostrarRonda() {
+  let i = 0
+  let movimientos = setInterval(function () {
+    jugadaMaquina[i];
+    
+    i++;
+    if (i>= jugadaMaquina.length) {
+      clearInterval(movimientos);
+      switch (i) {
+        case 0:
+          quitarSeleccionado('.rojo')
+        break;
+        case 1:
+          quitarSeleccionado('.verde')
+        break;
+        case 2:
+          quitarSeleccionado('.azul')
+        break;
+        case 3:
+          quitarSeleccionado('.amarillo')
+        break;
+      }
+    }
+  }, 600)
+}
+
+
+
+
+document.addEventListener(`input`,generarRondas) */
+
+////////////////////////////JUGADAS PERSONA
+
+// necesito una funcion que registre los clicks del jugador
+
+function clickPersona() {
+  
+  document.querySelector(".rojo").addEventListener("click",function() {
+    esRojo = true;
+    incorporarSeleccionado()
+    console.log(jugada)
+      return jugada
+      
+    
+  }) 
+  document.querySelector(".verde").addEventListener("click",function() {
+    esVerde = true;
+    incorporarSeleccionado()
+    console.log(jugada)
+    return jugada
+      
+    
+  })
+  document.querySelector(".azul").addEventListener("click",function() {
+    esAzul = true;
+    incorporarSeleccionado()
+    console.log(jugada)
+      return jugada
+      
+    
+  })
+  document.querySelector(".amarillo").addEventListener("click",function() {
+    esAmarillo = true;
+    incorporarSeleccionado()
+    console.log(jugada)
+      return jugada
+      
+    
+  })
+  } 
+
+// necesito una funcion que dependiendo cual fue el click del usuario incorpore ese valor al array jugada.
+
+function incorporarSeleccionado() {
+
+    if (esRojo == true) {
+      esRojo = false
+      return jugada.push(0);  
+    } else if (esVerde == true) {
+      esVerde = false
+      return jugada.push(1)
+    } else if (esAzul == true) {
+      esAzul = false
+      return jugada.push(2)
+    } else if (esAmarillo == true) {
+      esAmarillo = false
+      return jugada.push(3)
+  }
+}
+incorporarSeleccionado()
+console.log(jugada)
+
+// necesito una funcion que compare el array jugada con jugadaMaquina indice a indice, o al menos luego de finalizado el length correspondiente a esa ronda. La funcion deberia tomar como parametro el numero de ronda en la que estoy. A su vez al finalizar cada ronda, debe vaciar el array jugada.
+
+  function comparar() {
+    if (jugada.length === jugadaMaquina.length) {
+      let comparacion = jugada.every((valor,indice) => valor === jugadaMaquina[indice])
+      if (comparacion) {
+        return alert(`Excelente, lo lograste.`)
+      } else {
+        return alert(`Perdiste..`)
+      }
+    }
+  }
+  
+
+/*
+ Que me falta?
+
+ En cada jugada debe marcarse el indice de la maquina.
+ Si supero el primer nivel, el juego debe adaptar la comparacion al segundo nivel y luego al tercer nivel.
+ Aplicar JSON
+
+ */
+
+/* function clickPersona() {
+  document.querySelector(".rojo").addEventListener("click",function() {
+    reproducirAudio(audioRojo);
+    document.querySelector('.rojo').classList.remove('seleccionado')
+    setTimeout(function () {document.querySelector('.rojo').classList.add('seleccionado')},200)
+    jugada.push('rojo');
+    comparar(jugada,jugadaMaquina)
+  }) 
+  document.querySelector(".verde").addEventListener("click",function() {
+    reproducirAudio(audioVerde);
+    document.querySelector('.verde').classList.remove('seleccionado')
+    setTimeout(function () {document.querySelector('.verde').classList.add('seleccionado')},200)
+    jugada.push('verde');
+    comparar(jugada,jugadaMaquina)
+  })
+  document.querySelector(".azul").addEventListener("click",function() {
+    reproducirAudio(audioAzul);  
+    document.querySelector('.azul').classList.remove('seleccionado')
+    setTimeout(function () {document.querySelector('.azul').classList.add('seleccionado')},200)
+    jugada.push('azul');
+    comparar(jugada,jugadaMaquina)
+  })
+  document.querySelector(".amarillo").addEventListener("click",function() {
+    reproducirAudio(audioAmarillo);  
+    document.querySelector('.amarillo').classList.remove('seleccionado')
+    setTimeout(function () {document.querySelector('.amarillo').classList.add('seleccionado')},200)
+    jugada.push('amarillo');
+    comparar(jugada,jugadaMaquina)
+  })
+  }
+clickPersona(); */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* document.querySelector(".verde").addEventListener("click",function() {
+  jugada.push(1)
+  console.log(jugada)
+  if (jugada[0] == jugadaMaquina[0]) {
+    console.log("acertaste")
+    jugada = []
+  } else {
+    console.log("perdiste")
+  }
+})
+
+document.querySelector(".azul").addEventListener("click",function() {
+  jugada.push(2)
+  console.log(jugada)
+  if (jugada[0] == jugadaMaquina[0]) {
+    console.log("acertaste")
+    jugada = []
+  } else {
+    console.log("perdiste")
+  }
+})
+
+document.querySelector(".amarillo").addEventListener("click",function() {
+  jugada.push(3)
+  console.log(jugada)
+  if (jugada[0] == jugadaMaquina[0]) {
+    console.log("acertaste")
+    jugada = []
+  } else {
+    console.log("perdiste")
+  }
+}) */
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* //JUGADA PERSONA 1
+
+document.querySelector(".rojo").addEventListener("click",function() {
+  jugada.push(0)
+  console.log(jugada)
+  if (jugada[0] == jugadaMaquina[0]) {
+    console.log("acertaste")
+    jugada = []
+  } else {
+    console.log("perdiste")
+  }
+})
+
+document.querySelector(".verde").addEventListener("click",function() {
+  jugada.push(1)
+  console.log(jugada)
+  if (jugada[0] == jugadaMaquina[0]) {
+    console.log("acertaste")
+    jugada = []
+  } else {
+    console.log("perdiste")
+  }
+})
+
+document.querySelector(".azul").addEventListener("click",function() {
+  jugada.push(2)
+  console.log(jugada)
+  if (jugada[0] == jugadaMaquina[0]) {
+    console.log("acertaste")
+    jugada = []
+  } else {
+    console.log("perdiste")
+  }
+})
+
+document.querySelector(".amarillo").addEventListener("click",function() {
+  jugada.push(3)
+  console.log(jugada)
+  if (jugada[0] == jugadaMaquina[0]) {
+    console.log("acertaste")
+    jugada = []
+  } else {
+    console.log("perdiste")
+  }
+})
+
+
+
+/* new Promise(() => {
+  document.querySelector(".rojo").addEventListener("click",seleccionado)
+    
+  function seleccionado () {
+    consolidado.push(0)
+  }
+
+})
+ */
+
+
+
